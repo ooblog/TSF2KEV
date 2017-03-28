a651cabfc717828031a967007106b1d3afa5c5fd
ooblog <ooblog@gmail.com>
2017-03-28 21:33:34 +0900

    「TSF_Forth.d」と「TSF_Forth.py」で「TSF_Forth_view()」搭載。「TSF_Forth_init()」の動作も確認。

--- TSF_Forth.d
+++ TSF_Forth.d
@@ -31,7 +31,8 @@ void TSF_Forth_Initcards(ref string function()[string] TSF_cardsD,ref string[] T
 
 void function(ref string function()[string],ref string[])[] TSF_Initcards=[&TSF_Forth_Initcards];
 string function()[string] TSF_cardD=null;
-string[string] TSF_stackD=null,TSF_styleD=null;
+string[] [string] TSF_stackD=null;
+string [string] TSF_styleD=null;
 long[string] TSF_callptrD=null;
 string[] TSF_cardO=[],TSF_stackO=[],TSF_styleO=[],TSF_callptrO=[];
 string TSF_stackthis=TSF_Forth_1ststack(),TSF_stackthat=TSF_Forth_1ststack();
@@ -39,10 +40,12 @@ long TSF_stackcount=0;
 void TSF_Forth_init(string[] TSF_argvs,void function(ref string function()[string],ref string[])[] TSF_addcalls){
 //    TSF_stackD=null,TSF_styleD=null,TSF_callptrD=null,TSF_cardD=null;
     TSF_cardD=null;
-    TSF_stackD=null,TSF_styleD=null;
+    TSF_stackD=null;
+    TSF_styleD=null;
     TSF_callptrD=null;
     TSF_cardO,TSF_stackO=[],TSF_styleO=[],TSF_callptrO=[];
     TSF_stackthis=TSF_Forth_1ststack(),TSF_stackthat=TSF_Forth_1ststack();
+    TSF_stackD[TSF_stackthis]=["0","#TSF_fin."]; TSF_stackO~=[TSF_stackthis];
     void function(ref string function()[string],ref string[])[]  TSF_Initcards=[&TSF_Forth_Initcards];
     TSF_stackthis=TSF_Forth_1ststack(),TSF_stackthat=TSF_Forth_1ststack();
     TSF_stackcount=0;
@@ -61,7 +64,16 @@ string TSF_Forth_view(string TSF_the,bool TSF_view_io, ...){
             TSF_view_log=va_arg!(string)(_argptr);
         }
     }
-    if(TSF_the in TSF_stackD){
+    std.stdio.writeln(format("TSF_Forth_view:%s",TSF_the));
+    if( TSF_the in TSF_stackD ){
+        string TSF_style=(TSF_the in TSF_styleD)?TSF_styleD[TSF_the]:"T";
+        string TSF_view_logline="";
+        switch( TSF_style ){
+            case "O":  TSF_view_logline=format("%s\t%s\n",TSF_the,join(TSF_stackD[TSF_the],"\t"));  break;
+            case "T":  TSF_view_logline=format("%s\n\t%s\n",TSF_the,join(TSF_stackD[TSF_the],"\t"));  break;
+            case "N": default:  TSF_view_logline=format("%s\n\t%s\n",TSF_the,join(TSF_stackD[TSF_the],"\n\t"));  break;
+        }
+        TSF_view_log=(TSF_view_io)?TSF_Io_printlog(TSF_view_logline,TSF_view_log):TSF_view_log~TSF_view_logline;
     }
     return TSF_view_log;
 }
@@ -69,16 +81,11 @@ string TSF_Forth_view(string TSF_the,bool TSF_view_io, ...){
 void function(ref string function()[string],ref string[])[] TSF_Initcalldebug=[&TSF_Forth_Initcards];
 string TSF_Forth_debug(string[] TSF_argvs){
     string TSF_debug_log="";  string TSF_debug_savefilename="debug/debug_dForth.log";
-//    writeln(TSF_Initcalldebug[0]());
     std.stdio.writeln(format("--- %s ---",__MODULE__));
     TSF_Forth_init(TSF_argvs,TSF_Initcalldebug);
-//#    TSF_Forth_setTSF(TSF_Forth_1ststack(),"\t".join(["UTF-8","#TSF_encoding","0","#TSF_fin."]))
-//#    TSF_Forth_setTSF("TSF_Forth.py:","\t".join(["Python{0.major}.{0.minor}.{0.micro}".format(sys.version_info),sys.platform,TSF_io_stdout]))
-//#    TSF_Forth_setTSF("TSF_words:","\t".join(TSF_words.keys()))
-//#    TSF_Forth_addfin(TSF_argvs)
-//#    TSF_Forth_run()
-//#    for TSF_thename in TSF_Forth_stackskeys():
-//#        TSF_debug_log=TSF_Forth_view(TSF_thename,True,TSF_debug_log)
+    foreach(string TSF_the;TSF_stackO){
+        TSF_debug_log=TSF_Forth_view(TSF_the,true,TSF_debug_log);
+    }
     std.stdio.writeln(format("--- fin. > %s ---",TSF_debug_savefilename));
     TSF_Io_savetext(TSF_debug_savefilename,TSF_debug_log);
     return TSF_debug_log;
--- TSF_Forth.py
+++ TSF_Forth.py
@@ -24,7 +24,8 @@ def TSF_Forth_Initcards(TSF_cardsD,TSF_cardsO):    #TSF_doc:ワードを初期�
 
 TSF_Initcards=[]
 TSF_cardD={}
-TSF_stackD,TSF_styleD={},{}
+TSF_stackD={}
+TSF_styleD={}
 TSF_callptrD={}
 TSF_cardO,TSF_stackO,TSF_styleO,TSF_callptrO=[],[],[],[]
 TSF_stackthis,TSF_stackthat=TSF_Forth_1ststack(),TSF_Forth_1ststack()
@@ -33,13 +34,14 @@ def TSF_Forth_init(TSF_argvs=[],TSF_addcards=[]):    #TSF_doc:TSF_stacks,TSF_sty
     global TSF_stackD,TSF_styleD,TSF_callptrD,TSF_cardD,TSF_stackO,TSF_styleO,TSF_callptrO,TSF_cardO
     global TSF_stackthis,TSF_stackthat,TSF_stackcount
     TSF_cardD={}
-    TSF_stackD,TSF_styleD={},{}
+    TSF_stackD={}
+    TSF_styleD={}
     TSF_callptrD={}
     TSF_cardO,TSF_stackO,TSF_styleO,TSF_callptrO=[],[],[],[]
     TSF_stackthis,TSF_stackthat=TSF_Forth_1ststack(),TSF_Forth_1ststack()
     TSF_stackcount=0
     TSF_stackD[TSF_stackthis]=["0","#TSF_fin."]; TSF_stackO.append(TSF_stackthis)
-    TSF_stackD["#TSF_argvs"]=TSF_argvs[1:]; TSF_stackO.append("#TSF_argvs")
+#    TSF_stackD["#TSF_argvs"]=TSF_argvs[1:]; TSF_stackO.append("#TSF_argvs")
     TSF_Initcards=[TSF_Forth_Initcards]+TSF_addcards
     for TSF_Initcall in TSF_Initcards:
         TSF_cardD,TSF_cardO=TSF_Initcall(TSF_cardD,TSF_cardO)
@@ -66,14 +68,8 @@ def TSF_Io_debug(TSF_argvs):    #TSFdoc:「TSF/TSF_io.py」単体テスト風デ
     TSF_debug_log="";  TSF_debug_savefilename="debug/debug_pyForth.log";
     print("--- {0} ---".format(__file__))
     TSF_Forth_init(TSF_argvs,TSF_Initcalldebug)
-    TSF_Forth_view(TSF_Forth_1ststack(),TSF_view_io=True)
-#    TSF_Forth_setTSF(TSF_Forth_1ststack(),"\t".join(["UTF-8","#TSF_encoding","0","#TSF_fin."]))
-#    TSF_Forth_setTSF("TSF_Forth.py:","\t".join(["Python{0.major}.{0.minor}.{0.micro}".format(sys.version_info),sys.platform,TSF_io_stdout]))
-#    TSF_Forth_setTSF("TSF_words:","\t".join(TSF_words.keys()))
-#    TSF_Forth_addfin(TSF_argvs)
-#    TSF_Forth_run()
-#    for TSF_thename in TSF_Forth_stackskeys():
-#        TSF_debug_log=TSF_Forth_view(TSF_thename,True,TSF_debug_log)
+    for TSF_the in TSF_stackO:
+        TSF_debug_log=TSF_Forth_view(TSF_the,True,TSF_debug_log)
     print("--- fin. > {0} ---".format(TSF_debug_savefilename))
     TSF_Io_savetext(TSF_debug_savefilename,TSF_debug_log)
     return TSF_debug_log
