2e296f0d4ae46a97f79bcfcc308bf85e6f9884b3
ooblog <ooblog@gmail.com>
2017-05-13 06:57:02 +0900

    「winesh」フォルダ内のスクリプトを「winesh/TSF_DMDcompileEXE.sh」「winesh/TSFexe.sh」「winesh/TSFd.sh」「winesh/TSFpy.sh」に絞る。

---
+++ winesh/TSF_DMDcompileEXE.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd $script_dir
+cd ..
+
+/mnt/home/Tahrpup605/wine-portable-1.7.18-1-p4/wine-portable "dmd TSF.d TSF_Io.d TSF_Forth.d TSF_Shuffle.d TSF_Calc.d TSF_Match.d TSF_Trans.d"
