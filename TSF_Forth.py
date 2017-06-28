5804fa468ad22a7b6205149df99f396358a496c8
ooblog <ooblog@gmail.com>
2017-06-28 16:59:50 +0900

    ブランチ「20170628W165418」作成。「TSF_Shuffle.*」と「TSF_Io.*」とに分散してるPPPP関連を「TSF_Forth.*」に統合予定。

--- TSF_Forth.py
+++ TSF_Forth.py
@@ -9,7 +9,7 @@ def TSF_Forth_1ststack():    #TSFdoc:最初のスタック名(TSFAPI)。
     return "TSF_Tab-Separated-Forth:"
 
 def TSF_Forth_branchID():    #TSFdoc:TSFブランチ名(TSFAPI)。
-    return "20170528U134416"
+    return "20170628W165418"
 
 def TSF_Forth_grammarID():    #TSFdoc:TSF文法管理番号(TSFAPI)。
     return "2"
