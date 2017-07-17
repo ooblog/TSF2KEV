9b3f5003ef0f1fb5ff3b90618269e415f69fe01f
ooblog <ooblog@gmail.com>
2017-07-18 07:46:21 +0900

    ブランチ「20170718T073839」を作成。Pythonのdecimalモジュール使用箇所を多倍長整数に置き換えて大丈夫なのかテストする予定。

--- TSF_Forth.d
+++ TSF_Forth.d
@@ -22,7 +22,7 @@ string TSF_Forth_1ststack(){    //#TSFdoc:最初のスタック名(TSFAPI)。
 }
 
 string TSF_Forth_branchID(){    //#TSFdoc:TSFブランチ名(TSFAPI)。
-    return "20170703M004636";
+    return "20170718T073839";
 }
 
 string TSF_Forth_grammarID(){    //#TSFdoc:TSF文法管理番号(TSFAPI)。
